a6577ce07d123de91f9d88dd84b38efa7f78d761
Patrick Stoica <pstoica@users.noreply.github.com>
2023-08-11 16:28:56 -0400

    boop

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,9 +30,6 @@
 			<li>
 				<a href="https://instagram.com/patark">instagram</a>
 			</li>
-			<li>
-				<a href="https://deprogramming.substack.com/">substack</a>
-			</li>
 		</ul>
 		<a href="/" class="title text-6xl" 
 		style="background-position: -55px calc(50% - {y * 0.6}px);"	
